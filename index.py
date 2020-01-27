de662b42ab71260b431e00132fd59b3b07c91806
overspend <devantalian72@gmail.com>
2020-01-27 03:21:51 +0000

    add delete function

--- index.py
+++ index.py
@@ -1,52 +1,63 @@
-print("Welcome to Python Cli-Todolist!\n")
-print("1. Add todo\n2. View todolist")
-chooseMode = input("숫자를 입력해주세요: ")
+print("Welcome to Python Cli-Todolist!\n\n1. Add todo\n2. View todolist\n3. Delete todo\n")
+chooseMode = input("Enter Number: ")
 print("")
   
-def getLines():
-    readFile = open("index.txt", 'r')
-    readFileLength = str(len(readFile.readlines()) + 1)
-    readFile.close()
-    return readFileLength
     
-def saveTodo(Item):
-    setTodoItem = getLines() + ": " + Item + "\n"
-    todoTextFile = open("index.txt", 'a')
-    todoTextFile.write(setTodoItem)
-    todoTextFile.close()
-    afterAddTodo()
+def getLines():
+    with open("index.txt", 'r') as readFile:
+        readFileLength = str(len(readFile.readlines()) + 1)
+    return readFileLength
     
 def getTodoItem():
     todoItem = input("Enter Todo: ")
     saveTodo(todoItem)
     
-def afterAddTodo():
-    print("\nAdd Success!!\n\nWhat do you want to next?\n")
-    print("1. Add again\n2. View todolist\n3. Exit")
-    chooseAfterAddTodo = input("숫자를 입력해주세요: ")
+def saveTodo(Item):
+    setTodoItem = getLines() + ". " + Item + "\n"
+    with open("index.txt", 'a') as todoTextFile:
+        todoTextFile.write(setTodoItem)
+    afterSomeTodo()
+    
+def afterSomeTodo():
+    print("\n\nWhat do you want to next?\n\n1. Add again\n2. View todolist\n3. Delete Todo\n4. Exit\n")
+    chooseAfterSomeTodo = input("Enter Number: ")
     print("")
-    if(chooseAfterAddTodo == "1"):
+    if(chooseAfterSomeTodo == '1'):
         getTodoItem()
-    elif(chooseAfterAddTodo == "2"):
+    elif(chooseAfterSomeTodo == '2'):
         showTodoItems()
+    elif(chooseAfterSomeTodo == '3'):
+        delTodo()
     else:
         return
     
 def showTodoItems():
-    todoTextFile = open("index.txt", "r")
-    while True:
-        todoTextLine = todoTextFile.readline()
-        if not todoTextLine: break
-        print(todoTextLine)
-    todoTextFile.close()
-
+    with open("index.txt", 'r') as todoTextFile:
+        while True:
+            todoTextLine = todoTextFile.readline()
+            if not todoTextLine: break
+            print(todoTextLine)
+    
+def delTodo():
+    showTodoItems()
+    chooseDelWhat = input("Enter Todo You Want: ")
+    
+    with open("index.txt", 'r') as readFile:
+        readFileLines = readFile.readlines()
+    with open("index.txt", 'w') as readFile:
+        for line in readFileLines:
+            if line.strip("\n") != str(chooseDelWhat):
+                readFile.write(line)
+    
 class init():
     if(chooseMode == "1"):
         getTodoItem()
     elif(chooseMode == "2"):
         showTodoItems()
+    elif(chooseMode == "3"):
+        delTodo()
     else:
-        getNumber()
+        print("Please try again")
               
 init()
     
